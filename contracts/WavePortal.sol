6c0696765d810355071540120a4802653614af0b
Davit Mkhitaryan <davitmkhitaryan@Davits-MacBook-Pro.local>
2022-07-07 15:57:26 +0400

    Added wave, get total waves and get addresses' total waves functionality

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,14 +5,17 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    mapping(address => uint256) usersTotalWaves;
 
     constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+        console.log("Buildspace Web3 Learning Smart Contract");
     }
 
     function wave() public {
         totalWaves += 1;
+        usersTotalWaves[msg.sender] += 1;
         console.log("%s has waved!", msg.sender);
+        console.log("%s has a total of %s waves", msg.sender, usersTotalWaves[msg.sender]);
     }
 
     function getTotalWaves() public view returns (uint256) {
